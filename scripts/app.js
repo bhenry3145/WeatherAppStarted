d9acc4ec1c91af3727906a672d13af2b9bbcb1f5
bhenry3145 <bhenry3145@gmail.com>
2024-12-10 15:11:53 -0800

    changed functions to async functions

--- scripts/app.js
+++ scripts/app.js
@@ -4,38 +4,56 @@ let lon = '';
 let APIkey = '';
 let location = '';
 
-function myAPICall () {
-    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=37.9577&lon=-121.2908&appid=${APIKEY}`)
-    .then((response) => {
-        return response.json()
-    })
-    .then((data) => {
-        console.log(data);
-    })
+// function myAPICall() {
+//     fetch(`https://api.openweathermap.org/data/2.5/weather?lat=37.9577&lon=-121.2908&appid=${APIKEY}`)
+//     .then((response) => {
+//         return response.json()
+//     })
+//     .then((data) => {
+//         console.log(data);
+//     })
+// }
+
+async function myAPICall() {
+    const promise = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=37.9577&lon=-121.2908&appid=${APIKEY}`);
+    const data = await promise.json();
+    console.log(data);
 }
 
 // myAPICall();
 
-function fiveDayWeatherAPI() {
-    fetch(`api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${APIkey}`)
-    .then((response) => {
-        return response.json()
-    })
-    .then((data) => {
-        console.log(data);
-    })
+// function fiveDayWeatherAPI() {
+//     fetch(`api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${APIkey}`)
+//     .then((response) => {
+//         return response.json()
+//     })
+//     .then((data) => {
+//         console.log(data);
+//     })
+// }
+
+async function fiveDayWeatherAPI() {
+    const promise = await fetch(`api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${APIkey}`);
+    const data = await promise.json();
+    console.log(data);
 }
 
 // fiveDayWeatherAPI();
 
-function geocodingAPI() {
-    fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${location}&limit=1&appid=${APIkey}`)
-    .then((response) => {
-        return response.json()
-    })
-    .then((data) => {
-        console.log(data);
-    })
+// function geocodingAPI() {
+//     fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${location}&limit=1&appid=${APIkey}`)
+//     .then((response) => {
+//         return response.json()
+//     })
+//     .then((data) => {
+//         console.log(data);
+//     })
+// }
+
+async function geocodingAPI() {
+    const promise = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${location}&limit=1&appid=${APIkey}`);
+    const data = await promise.json();
+    console.log(data);
 }
 
 // geocodingAPI();
