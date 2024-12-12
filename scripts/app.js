025be04f0c40541416719bfd5d4aa623925a3e58
bhenry3145 <bhenry3145@gmail.com>
2024-12-12 10:02:32 -0800

    fixing data

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,4 @@
 import { APIKEY } from "./environment.js";
-let lat = "48.1486";
-let lon = '17.1077';
-let location = 'Bratislava, Slovakia';
 
 let searchInput = document.getElementById('searchInput');
 let stocktonWeather = document.getElementById('stocktonWeather');
@@ -42,7 +39,7 @@ async function myAPICall() {
 
 // myAPICall();
 
-async function fiveDayWeatherAPI() {
+async function fiveDayWeatherAPI(lat, lon) {
     const promise = await fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${APIKEY}`);
     const dataFive = await promise.json();
     console.log(dataFive);
@@ -51,7 +48,7 @@ async function fiveDayWeatherAPI() {
 
 // fiveDayWeatherAPI();
 
-async function geocodingAPI() {
+async function geocodingAPI(location) {
     const promise = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${location}&limit=1&appid=${APIKEY}`);
     const dataGeo = await promise.json();
     console.log(dataGeo);
@@ -96,37 +93,42 @@ async function stocktonWeatherFunc() {
     date.innerText = `${monthArr[month]}/${dayOfMonth}/${year}`
 }
 
-searchInput.addEventListener('keypress', function(event) {
+searchInput.addEventListener('keydown', async function(event) {
     if (event.key == "Enter")
     {
         let location = searchInput.value;
-        console.log(`Location set to: ${location}`);
-        console.log(lat);
-        console.log(lon);
-        fiveDayWeatherAPI();
-        let temp = (dataFive.list[0].main.temp + dataFive.list[1].main.temp + dataFive.list[2].main.temp + dataFive.list[3].main.temp + dataFive.list[4].main.temp + dataFive.list[5].main.temp + dataFive.list[6].main.temp + dataFive.list[7].main.temp) / 8;
-        let maxTemp = (dataFive.list[0].main.temp_max + dataFive.list[1].main.temp_max + dataFive.list[2].main.temp_max + dataFive.list[3].main.temp_max + dataFive.list[4].main.temp_max + dataFive.list[5].main.temp_max + dataFive.list[6].main.temp_max + dataFive.list[7].main.temp_max) / 8;
-        let minTemp = (dataFive.list[0].main.temp_min + dataFive.list[1].main.temp_min + dataFive.list[2].main.temp_min + dataFive.list[3].main.temp_min + dataFive.list[4].main.temp_min + dataFive.list[5].main.temp_min + dataFive.list[6].main.temp_min + dataFive.list[7].main.temp_min) / 8;
+        let geoData = await geocodingAPI(location);
+        console.log(geoData);
+        const dataFive = await fiveDayWeatherAPI(geoData[0].lat, geoData[0].lon);
+        console.log(dataFive);
+        searchFunc(dataFive);
     }
 })
 
-async function searchFunc() {
-    let dataGeo = await geocodingAPI();
-    let lat = dataGeo[0].lat;
-    let lon = dataGeo[0].lon;
-    let ms = dataGeo.dt * 1000;
+function searchFunc(dataFive) {
+    console.log(dataFive);
+    let ms = dataFive.dt * 1000;
     let newDate = new Date(ms);
     let month = newDate.getMonth();
     let monthArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
     let year = newDate.getFullYear()
     let dayOfMonth = newDate.getDate();
-    const kelvinToFahrenheit = (kelvin) => ((kelvin - 273.15) * 9/5 + 32).toFixed(0);
-    let temp = kelvinToFahrenheit(dataGeo.main.temp);
-    let tempMax = kelvinToFahrenheit(dataGeo.main.temp_max);
-    let tempMin = kelvinToFahrenheit(dataGeo.main.temp_min);
-    temperature.innerText = `${temp}°F`;
-    maxTemp.innerText = `Max: ${tempMax}°F`;
-    minTemp.innerText = `Min: ${tempMin}°F`;
+    temperature.innerText = `${(dataFive.list[0].main.temp + dataFive.list[1].main.temp + dataFive.list[2].main.temp + dataFive.list[3].main.temp + dataFive.list[4].main.temp + dataFive.list[5].main.temp + dataFive.list[6].main.temp + dataFive.list[7].main.temp) / 8}°F`;
+    maxTemp.innerText = `Max: ${(dataFive.list[0].main.temp_max + dataFive.list[1].main.temp_max + dataFive.list[2].main.temp_max + dataFive.list[3].main.temp_max + dataFive.list[4].main.temp_max + dataFive.list[5].main.temp_max + dataFive.list[6].main.temp_max + dataFive.list[7].main.temp_max) / 8}°F`;
+    minTemp.innerText =`Min: ${(dataFive.list[0].main.temp_min + dataFive.list[1].main.temp_min + dataFive.list[2].main.temp_min + dataFive.list[3].main.temp_min + dataFive.list[4].main.temp_min + dataFive.list[5].main.temp_min + dataFive.list[6].main.temp_min + dataFive.list[7].main.temp_min) / 8}°F`;
+    // currentWeatherIcon.src = `http://openweathermap.org/img/wn/${dataFive.weather[0].icon}@2x.png`;
+    // weatherDescription.innerText = `${dataMy.weather[0].description}`;
+    // cityName.innerText = `${dataMy.name}`;
+    date.innerText = `${monthArr[month]}/${dayOfMonth}/${year}`
+
+    temperature1.innerText = `${(dataFive.list[8].main.temp + dataFive.list[9].main.temp + dataFive.list[10].main.temp + dataFive.list[11].main.temp + dataFive.list[12].main.temp + dataFive.list[13].main.temp + dataFive.list[14].main.temp + dataFive.list[15].main.temp) / 8}°F`;
+
+    temperature2.innerText = `${(dataFive.list[16].main.temp + dataFive.list[17].main.temp + dataFive.list[18].main.temp + dataFive.list[19].main.temp + dataFive.list[20].main.temp + dataFive.list[21].main.temp + dataFive.list[22].main.temp + dataFive.list[23].main.temp) / 8}°F`;
+
+    temperature3.innerText = `${(dataFive.list[24].main.temp + dataFive.list[25].main.temp + dataFive.list[26].main.temp + dataFive.list[27].main.temp + dataFive.list[28].main.temp + dataFive.list[29].main.temp + dataFive.list[30].main.temp + dataFive.list[31].main.temp) / 8}°F`;
+
+    temperature4.innerText = `${(dataFive.list[32].main.temp + dataFive.list[33].main.temp + dataFive.list[34].main.temp + dataFive.list[35].main.temp + dataFive.list[36].main.temp + dataFive.list[37].main.temp + dataFive.list[38].main.temp + dataFive.list[39].main.temp) / 8}°F`;
+
 }
 
 // stocktonWeather.addEventListener('click', function() {
